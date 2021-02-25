8380c6303932c5bcaf3eb311fc5b4b0cb0edba54
Sanju Uthaiah Bollera <bollera.sanju@outlook.com>
2021-02-25 09:55:30 +0530

    Initial commit

---
+++ src/Carousel.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+interface Props {
+  onChange: Function;
+}
+
+const Carousel: React.FC<Props> = ({ onChange }) => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+export default Carousel;
